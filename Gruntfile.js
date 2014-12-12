c35ba9fb3bcce08daa94d222ec0b99f4154d1585
Damian Galarza and Tute Costa <support+Damian+Tute@thoughtbot.com>
2014-12-12 14:12:07 -0500

    Set up grunt watch to run specs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,15 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON("package.json"),
     jasmine_node: {
       all: ["spec/"]
+    },
+    watch: {
+      files: ["spec/**/*", "lib/**/*"],
+      tasks: ["jasmine_node"]
     }
   });
 
   grunt.loadNpmTasks("grunt-jasmine-node");
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask("default", ["jasmine_node"]);
 };
